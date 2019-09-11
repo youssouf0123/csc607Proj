33acac569def58ac8827296f4143371a2aa54b4b
dcamara3 <dcamara3@gmail.com>
2019-09-10 20:31:47 -0400

    added a new library to present a web audio player

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Track } from 'ngx-audio-player';   
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'csc607project';
+
+	displayVolumeControls = true;
+	msaapDisplayTitle = true;
+	msaapDisplayPlayList = true;
+	msaapPageSizeOptions = [2,4,6];
+	msaapDisplayVolumeControls = true;
+	   
+	// Material Style Advance Audio Player Playlist
+	msaapPlaylist: Track[] = [
+	  {
+	    title: 'Audio One Title',
+	    link: 'https://www.computerhope.com/jargon/m/example.mp3'
+	  },
+	  {
+	    title: 'Audio Two Title',
+	    link: 'https://www.computerhope.com/jargon/m/example.mp3'
+	  },
+	  {
+	    title: 'Audio Three Title',
+	    link: 'https://www.computerhope.com/jargon/m/example.mp3'
+	  },
+	];
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NgxAudioPlayerModule } from 'ngx-audio-player';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+	BrowserAnimationsModule,
+	NgxAudioPlayerModule,
     BrowserModule,
     AppRoutingModule
   ],
